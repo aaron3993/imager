5e49d39080a45ca8ea8c2ed9429ed0fea2251567
Aaron <aaron3993@hotmail.com>
2021-05-02 21:39:29 -0400

    Wrapped search functionality into a form to press enter

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, FormGroup, Input } from "reactstrap";
+import { Button, Form, FormGroup, Input } from "reactstrap";
 import axios from "axios";
 
 import ImageList from "./ImageList";
@@ -9,36 +9,37 @@ const Home = (props) => {
   const [images, setImages] = useState([]);
   const [search, setSearch] = useState("");
 
-  const searchImages = async (term) => {
+  const searchImages = async (e, term) => {
+    e.preventDefault();
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID HogGshsiM9hbZC7IBAJ1opNRJb9aWwQEoEVrCsxxYq8",
       },
     });
-    console.log(response);
     setImages(response.data.results);
   };
 
   return (
-    <div className="mt-3 d-flex flex-column align-items-center">
-      <FormGroup>
-        <Input
-          type="text"
-          placeholder="Search Images"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Button
-          color="primary"
-          type="submit"
-          onClick={() => searchImages(search)}
-        >
-          Search
-        </Button>
-      </FormGroup>
+    <div>
+      <Form
+        className="mt-3 d-flex flex-column align-items-center"
+        onSubmit={(e) => searchImages(e, search)}
+      >
+        <FormGroup>
+          <Input
+            type="text"
+            placeholder="Search Images"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Button color="primary" type="submit">
+            Search
+          </Button>
+        </FormGroup>
+      </Form>
       {/* {!images.length ? (
         <Welcome />
       ) : (
